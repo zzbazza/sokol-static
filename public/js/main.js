ff1922d279d162ed25cd2942092d2ce8c68d2cab
zzbazza <jlafek@seznam.cz>
2025-03-21 15:01:33 +0100

    add volejbal, vybor foto

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,18 @@
 // import lgThumbnail from 'lightgallery/plugins/thumbnail'
 // import lgZoom from 'lightgallery/plugins/zoom'
 
+document.addEventListener('DOMContentLoaded', function () {
+  console.dir(document.getElementById('lightgallery'))
+  lightGallery(document.getElementById('lightgallery'), {
+    autoplayFirstVideo: false,
+    pager: false,
+    galleryId: "nature",
+    plugins: [lgZoom, lgThumbnail],
+    mobileSettings: {
+      controls: false,
+      showCloseIcon: false,
+      download: false,
+      rotate: false
+    }
+  });
+});
